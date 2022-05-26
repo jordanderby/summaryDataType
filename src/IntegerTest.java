8e078d66e093078ec73716b99f1708f910e1bc33
jordanderby <106184719+jordanderby@users.noreply.github.com>
2022-05-26 15:47:48 -0400

    Create IntegerTest.java

---
+++ src/IntegerTest.java
@@ -0,0 +1,13 @@
+/** class name: IntegerTest.java
+* author: Jordan Derby
+* date: 5/26/2022
+* description: Tests int overflow
+*
+*/
+
+public class IntegerTest {
+  public static void main(String[] args){
+    int number = 2147483647;
+    System.out.print(number + 1);
+  }
+}
