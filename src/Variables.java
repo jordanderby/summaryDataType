76240c85b3f4c7ffe61409b7e749835ca594f61b
jordanderby <106184719+jordanderby@users.noreply.github.com>
2022-05-26 14:24:59 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,8 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
- * version:
+ * author: Jordan Derby
+ * date: 5/26/2022
+ * version: 1.0
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
  * direction: 1) read the source material
@@ -15,6 +15,23 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("Byte: 8-bit signed two complements int");
+        byte newByte = 1;
+        System.out.println("Short: 16-bit signed twos complements int");
+        short newShort = 1;
+        System.out.println("Int: 32-bit signed twos complements int");
+        int newInt = 1;
+        System.out.println("Long: 64-bit signed two complents int");
+        long newLong = 0L;
+        System.out.println("Float: single-precision 32-bit floating point");
+        float newFloat = 1.0;
+        System.out.println("Double: double-precision 64-bit floating point");
+        double newFloat = 1.1;
+        System.out.println("Boolean: a true or false condition");
+        boolean valid = true;
+        System.out.println("Char: a single unit unicode character");
+        char a;
+        
     }
 
 }
